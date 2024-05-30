f66b807fa7357bfc0591518da2aabbd391a90523
suhsein <mini9075@naver.com>
2024-05-30 18:34:54 +0900

    first commit

---
+++ test-security/src/main/java/com/example/test_security/TestSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSecurityApplication.class, args);
+	}
+
+}
